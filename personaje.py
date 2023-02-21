aa8baedb99fb093c60722b86e5ad36f8e021af2a
JR-B07 <121039413@upq.edu.mx>
2023-02-20 18:43:25 -0600

    Definir los metodos del personaje

--- personaje.py
+++ personaje.py
@@ -4,3 +4,19 @@ class Personaje
     nombre= "Augustus Cole"
     altura= 1.87
     
+    #METODOS PERSONAJE
+    
+    def correr(self,status):
+        if(status):
+            print("EL personaje " self.nombre + "esta corriendo " )
+        else:
+            print("EL personaje " self.nombre + "se detuvo" )
+            
+    def lanzarGranada(self):
+        print("se lanzo granada ")
+        
+    def recargarArma(self,municiones):
+        cargador= 5
+        cargador = cargador + municiones
+        print("El arma tiene ahora " + cargador + "balas")
+        
