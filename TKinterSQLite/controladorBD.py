f44bb689d0ead9be6cc036042c463412fd74c3c4
JR-B07 <121039413@upq.edu.mx>
2023-03-29 18:58:23 -0600

    practica 16

--- TKinterSQLite/controladorBD.py
+++ TKinterSQLite/controladorBD.py
@@ -28,6 +28,7 @@ def guardarUsuario(self,nom,cor,con):
         
         # 3 preparamos cursor,datos,querysql
         cursor= conx.cursor()
+        conH= self.encriptarCon(con)
         datos=(nom,cor,con)
         qrInsert= "insert into TBRegistrados(nombre,correo,contra) values(?,?,?)"
         
@@ -36,4 +37,41 @@ def guardarUsuario(self,nom,cor,con):
         conx.commit()
         conx.close
         messagebox.showinfo("Exito","Usuario Guardado")
+        
+#Metodo para encryptar contraseñas
+    def encriptarCon(self, con):
+        conPlana= con
+        
+        #metodo para encriptar contraseñas
+        conPlana= conPlana.encode() #convertimos con a bytes
+        sal= bcrypt.gensalt() #gensalt es algoritmo de aleatoriedad
+        conHA= bcrypt.hashpw(conPlana,sal)  #hashpw te da la contraseña encriptada
+        print(conHA)
+        #enviamos la contraseña encriptada
+        return conHA 
+    
+    # metodo para buscar 1 usuario
+    
+    def consultarUsuario(self,id):
+        #preparar una conexción
+        conx= self.conexionBD()
+        
+        #2. verificar si id contiene algo
+        if(id == ""):
+            messagebox.showwarning("cuidado", "Id vacio escribe algo valido")
+        else:
+            try:
+                #3. preparar consulta y el query
+                cursor= conx.cursor()
+                selectQry= "select * from TBRegistrados where id= "+id
+                
+                #4. ejecutar y guardar la consulta 
+                cursor.execute(selectQry)
+                rsUsuario= cursor.fetchall()
+                conx.close()
+                
+                return rsUsuario
+            
+            except sqlite3.OperationalError:
+                print("Error Consulta ")
         
